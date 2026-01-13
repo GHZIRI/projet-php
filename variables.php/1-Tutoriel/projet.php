96cdb9abcbdd4061be77c6940a82db726d28132f
GHZIRI <salmantanger1234@gmail.com>
2026-01-13 09:16:56 +0000

    tutoriel 1

---
+++ variables.php/1-Tutoriel/projet.php
@@ -0,0 +1,12 @@
+<?php
+$name = "salman";
+$age = 20;
+$prix = 19.99;
+$estConnecte = true;
+
+echo "Name : " . $name . "<br>";
+echo "Age : " . $age . "<br>";
+echo "Prix : " . $prix . "<br>";
+echo "Connecté : " . ($estConnecte ? "Oui" : "Non") . "<br>";
+
+var_dump($name);
