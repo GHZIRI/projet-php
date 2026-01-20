852f4d51c5230f56832401b7f1446218645cd496
GHZIRI <salmantanger1234@gmail.com>
2026-01-20 11:13:29 +0000

    add tutoriel 4

---
+++ variables.php/4-Tutoriel/table.php
@@ -0,0 +1,14 @@
+<?php
+$animaux = ["Chat", "Chien", "Lapin"];
+echo "Premier animal : " .$animaux[0]. "<br>";
+
+$voiture = [
+    "marque" => "Toyota",
+    "modele" => "Corolla",
+    "année" => 2020
+];
+echo "Modèle : " . $voiture["modele"] . "<br>";
+$fruits = ["Pomme", "Banane"];
+array_push($fruits, "Mangue"); // Ajoute "Mangue"
+unset($fruits[1]); // Supprime "Banane"
+echo "Nombre de fruits : " . count($fruits);
